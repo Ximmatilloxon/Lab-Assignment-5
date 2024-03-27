94d2bffe9c2d93517fae7fa88d9d81ace8bb51a8
Ximmatilloxon Musayev <x.musayev@newuzbekistanuniversity.uz>
2024-03-27 12:21:39 +0500

    Done!

--- problem2.cpp
+++ problem2.cpp
@@ -1,10 +1,21 @@
 #include "iostream"
-#include "cmath"
 using namespace std;
+
+int power(int base, int exp) {
+    int result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exp /= 2;
+    }
+    return result;
+}
 
 int def(int n){
     if(n == 1) return 3;
-    return (pow(def(n - 1), 2) + 3);
+    return power(def(n - 1), 2) + 3;
 }
 
 int main(){
